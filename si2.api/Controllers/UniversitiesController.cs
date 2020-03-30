089bb4b46aa88e17409311420c0a904edccc09c2
Marie Kassis <marie.kassis@usj.edu.lb>
2020-03-30 19:46:36 +0300

    Added new functions to UniversitiesController: deleteUniversity, getUniversities, updateUniversity...

--- si2.api/Controllers/UniversitiesController.cs
+++ si2.api/Controllers/UniversitiesController.cs
@@ -26,6 +26,12 @@ namespace si2.api.Controllers
         private readonly ILogger<UniversitiesController> _logger;
         private readonly IUniversityService _universityService;
 
+        /*
+         {
+            "token": "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJhYmNkXzEyM0Bob3RtYWlsLmNvbSIsImp0aSI6IjhjZjdmN2FlLTA0YzEtNDUxMC1hNzM1LTA3Yjg3Y2ZmZmZjYyIsInVuaXF1ZV9uYW1lIjoiYWJjZF8xMjNAaG90bWFpbC5jb20iLCJleHAiOjE1ODUyMjI5MDksImlzcyI6Ik1WUyIsImF1ZCI6IkFwaVVzZXIifQ.DfVpCHbUcvwfVqa-9FunHtn0KAkMGSQBBJefZbBecq4",
+            "expiration": "2020-03-26T11:41:49Z"
+        }*/
+
         public UniversitiesController(LinkGenerator linkGenerator, ILogger<UniversitiesController> logger, IUniversityService universityService)
 		{
             _linkGenerator = linkGenerator;
@@ -49,12 +55,19 @@ namespace si2.api.Controllers
         }*/
 
         [HttpGet("{id}", Name = "GetUniversity")]
+        [Authorize(AuthenticationSchemes = "Bearer")]
         [ProducesResponseType(StatusCodes.Status200OK, Type = typeof(UniversityDto))]
         [ProducesResponseType(StatusCodes.Status404NotFound)]
         public async Task<ActionResult> GetUniversity(Guid id, CancellationToken ct)
         {
+            var universityDto = await _universityService.GetUniversityByIdAsync(id, ct);
 
-            return Ok("Reached");
+            if (universityDto == null)
+                return NotFound();
+
+            return Ok(universityDto);
+
+            //return Ok("Reached");
         }
 
 
@@ -70,5 +83,113 @@ namespace si2.api.Controllers
 
             return CreatedAtRoute("GetUniversity", new { id = universityToReturn.Id }, universityToReturn);
         }
-    }
+
+        [HttpDelete("{id}")]
+        [Authorize(AuthenticationSchemes = "Bearer")]
+        [ProducesResponseType(StatusCodes.Status204NoContent)]
+        public async Task<ActionResult> DeleteUniversity(Guid id, CancellationToken ct)
+        {
+            await _universityService.DeleteUniversityByIdAsync(id, ct);
+
+            return NoContent();
+        }
+
+
+        [HttpGet(Name = "GetUniversities")]
+        public async Task<ActionResult> GetUniversities([FromQuery]UniversityResourceParameters pagedResourceParameters, CancellationToken ct)
+        {
+            var universityDtos = await _universityService.GetUniversitiesAsync(pagedResourceParameters, ct);
+
+            var previousPageLink = universityDtos.HasPrevious ? CreateUniversitiesResourceUri(pagedResourceParameters, Enums.ResourceUriType.PreviousPage) : null;
+            var nextPageLink = universityDtos.HasNext ? CreateUniversitiesResourceUri(pagedResourceParameters, Enums.ResourceUriType.NextPage) : null;
+
+            var paginationMetadata = new
+            {
+                totalCount = universityDtos.TotalCount,
+                pageSize = universityDtos.PageSize,
+                currentPage = universityDtos.CurrentPage,
+                totalPages = universityDtos.TotalPages,
+                previousPageLink,
+                nextPageLink
+            };
+
+            if (universityDtos == null)
+                return NotFound();
+
+            Response.Headers.Add("X-Pagination", JsonConvert.SerializeObject(paginationMetadata));
+            return Ok(universityDtos);
+        }
+
+        [HttpPut("{id}")]
+        [ProducesResponseType(StatusCodes.Status200OK, Type = typeof(UniversityDto))]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        public async Task<ActionResult> UpdateUniversity([FromRoute]Guid id, [FromBody] UpdateUniversityDto updateUniversityDto, CancellationToken ct)
+        {
+            if (!await _universityService.ExistsAsync(id, ct))
+                return NotFound();
+
+            var universityToReturn = await _universityService.UpdateUniversityAsync(id, updateUniversityDto, ct);
+            if (universityToReturn == null)
+                return BadRequest();
+
+            return Ok(universityToReturn);
+        }
+
+        [HttpPatch("{id}")]
+        public async Task<ActionResult> UpdateUniversity([FromRoute]Guid id, [FromBody] JsonPatchDocument<UpdateUniversityDto> patchDoc, CancellationToken ct)
+        {
+            if (!await _universityService.ExistsAsync(id, ct))
+                return NotFound();
+
+            var universityToPatch = await _universityService.GetUpdateUniversityDto(id, ct);
+            patchDoc.ApplyTo(universityToPatch, ModelState);
+
+            TryValidateModel(universityToPatch);
+
+            if (!ModelState.IsValid)
+                return new UnprocessableEntityObjectResult(ModelState);
+
+            var universityToReturn = await _universityService.PartialUpdateUniversityAsync(id, universityToPatch, ct);
+            if (universityToReturn == null)
+                return BadRequest();
+
+            return Ok(universityToReturn);
+        }
+
+
+        private string CreateUniversitiesResourceUri(UniversityResourceParameters pagedResourceParameters, Enums.ResourceUriType type)
+        {
+            switch (type)
+            {
+                case Enums.ResourceUriType.PreviousPage:
+                    return _linkGenerator.GetUriByName(this.HttpContext, "GetUniversities",
+                        new
+                        {
+                            status = pagedResourceParameters.Name,
+                            searchQuery = pagedResourceParameters.SearchQuery,
+                            pageNumber = pagedResourceParameters.PageNumber - 1,
+                            pageSize = pagedResourceParameters.PageSize
+                        }); // TODO get the aboslute path 
+                case Enums.ResourceUriType.NextPage:
+                    return _linkGenerator.GetUriByName(this.HttpContext, "GetUniversities",
+                        new
+                        {
+                            status = pagedResourceParameters.Name,
+                            searchQuery = pagedResourceParameters.SearchQuery,
+                            pageNumber = pagedResourceParameters.PageNumber + 1,
+                            pageSize = pagedResourceParameters.PageSize
+                        });
+                default:
+                    return _linkGenerator.GetUriByName(this.HttpContext, "GetUniversities",
+                       new
+                       {
+                           status = pagedResourceParameters.Name,
+                           searchQuery = pagedResourceParameters.SearchQuery,
+                           pageNumber = pagedResourceParameters.PageNumber,
+                           pageSize = pagedResourceParameters.PageSize
+                       });
+            }
+        }
+
+}
 }
---
+++ si2.dal/Migrations/20200325131743_Added_University_Entity.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace si2.dal.Migrations
+{
+    public partial class Added_University_Entity : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "University",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(nullable: false),
+                    RowVersion = table.Column<byte[]>(rowVersion: true, nullable: true),
+                    Name = table.Column<string>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_University", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "University");
+        }
+    }
+}
